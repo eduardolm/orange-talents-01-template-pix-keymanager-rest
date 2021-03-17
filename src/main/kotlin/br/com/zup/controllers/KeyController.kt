56cea721cb54712422cb18045852b0f2cdaee1a8
Eduardo Lodi Marzano <lodi001@uol.com.br>
2021-03-17 11:09:20 -0300

    Finished create pix key feature with request params validation

--- src/main/kotlin/br/com/zup/controllers/KeyController.kt
+++ src/main/kotlin/br/com/zup/controllers/KeyController.kt
@@ -3,10 +3,14 @@ package br.com.zup.controllers
 import br.com.zup.KeyServiceGrpc
 import br.com.zup.dto.request.KeyRequestDto
 import br.com.zup.service.GrpcKeyService
+import io.grpc.Status
+import io.grpc.StatusRuntimeException
 import io.micronaut.http.HttpResponse
+import io.micronaut.http.HttpStatus
 import io.micronaut.http.annotation.Body
 import io.micronaut.http.annotation.Controller
 import io.micronaut.http.annotation.Post
+import io.micronaut.http.exceptions.HttpStatusException
 import io.micronaut.validation.Validated
 import javax.inject.Inject
 import javax.validation.Valid
@@ -23,7 +27,26 @@ class KeyController(
 
         with(requestDto) {
             keyService.buildGrpcRequest(this)
-        }?.let { gRpcClient.create(it) }?.let { return HttpResponse.created(keyService.buildResponse(it)) }
+        }?.let {
+            try {
+                gRpcClient.create(it)
+            } catch (e: StatusRuntimeException) {
+
+                val status = e.status
+                val statusCode = e.status.code
+                val description =
+                    if (status.description.equals("io exception"))
+                        "Falha ao acessar o servidor." else status.description
+
+
+                if (statusCode == Status.Code.ALREADY_EXISTS) {
+                    throw HttpStatusException(HttpStatus.UNPROCESSABLE_ENTITY, description)
+                } else {
+                    throw HttpStatusException(HttpStatus.INTERNAL_SERVER_ERROR, description)
+                }
+            }
+
+        }?.let { return HttpResponse.created(keyService.buildResponse(it)) }
 
         return HttpResponse.notFound()
     }
--- src/main/kotlin/br/com/zup/validators/constraints/Key.kt
+++ src/main/kotlin/br/com/zup/validators/constraints/Key.kt
@@ -2,11 +2,23 @@ package br.com.zup.validators.constraints
 
 import br.com.zup.validators.KeyValidator
 import javax.validation.Constraint
+import javax.validation.Payload
 import javax.validation.ReportAsSingleViolation
+import kotlin.reflect.KClass
 
-@Target(AnnotationTarget.FIELD, AnnotationTarget.CONSTRUCTOR, AnnotationTarget.CLASS)
+@Target(
+    AnnotationTarget.FIELD,
+    AnnotationTarget.CONSTRUCTOR,
+    AnnotationTarget.CLASS,
+    AnnotationTarget.PROPERTY,
+    AnnotationTarget.VALUE_PARAMETER
+)
 @ReportAsSingleViolation
 @MustBeDocumented
 @Constraint(validatedBy = [KeyValidator::class])
 @kotlin.annotation.Retention(AnnotationRetention.RUNTIME)
-annotation class Key(val message:String = "Chave inválida.")
+annotation class Key(
+    val message: String = "Chave inválida.",
+    val groups: Array<KClass<Any>> = [],
+    val payload: Array<KClass<Payload>> = []
+)
