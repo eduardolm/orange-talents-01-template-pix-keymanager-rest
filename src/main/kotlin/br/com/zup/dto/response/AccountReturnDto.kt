53fda1463fc778dca140cbdd2e63c86baa520414
Eduardo Lodi Marzano <lodi001@uol.com.br>
2021-03-21 18:45:39 -0300

    Finished reatrieve pix by id or ke feature

--- src/main/kotlin/br/com/zup/dto/response/AccountReturnDto.kt
+++ src/main/kotlin/br/com/zup/dto/response/AccountReturnDto.kt
@@ -1,10 +1,11 @@
 package br.com.zup.dto.response
 
-import br.com.zup.model.Account
+data class AccountReturnDto(
+    val tipo: String,
+    val instituicao: OrganizationResponseDto,
+    val agencia: String,
+    val numero: String,
+    val titular: OwnerResponseDto
+) {
 
-data class AccountReturnDto(val tipo: String, val instituicao: OrganizationResponseDto, val agencia: String, val numero: String, val titular: OwnerResponseDto) {
-
-    fun toModel(): Account {
-        return Account(tipo, instituicao.toModel(), agencia, numero, titular.toModel())
-    }
 }
--- src/main/kotlin/br/com/zup/dto/response/OrganizationResponseDto.kt
+++ src/main/kotlin/br/com/zup/dto/response/OrganizationResponseDto.kt
@@ -1,10 +1,5 @@
 package br.com.zup.dto.response
 
-import br.com.zup.model.Organization
-
 data class OrganizationResponseDto(val nome: String, val ispb: String) {
 
-    fun toModel(): Organization {
-        return Organization(nome, ispb)
-    }
 }
